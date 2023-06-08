83ab98667c68dd6453bc0b117e1c8a95b0cb5501
Morgan Williams <69870486+morluv225@users.noreply.github.com>
2023-06-08 11:24:47 -0700

    Update start_window.py
    Added code from MorganChanges because merge didn't work.

--- gui/start_window.py
+++ gui/start_window.py
@@ -15,17 +15,19 @@ class StartWindow(QWidget):
         # Create filtering window
         self.filter_window = FilteringWindow()
         self.setWindowTitle("ClassMate")
-        self.setStyleSheet("background-color: darkcyan;")
+        self.setStyleSheet("background-color: azure;")  # previous color: darkcyan
         layout = QHBoxLayout()
 
         layout_v = QVBoxLayout()
         lbl = QLabel('Welcome to Classmate')
-        font = lbl.font()
-        font.setPointSize(30)
-        lbl.setFont(font)
+        #font = lbl.font()
+        #font.setPointSize(30)
+        lbl.setFont(QFont('Bodoni MT', 55))
         lbl.setAlignment(Qt.AlignHCenter | Qt.AlignBottom)
 
         button1 = QPushButton("Get Started")
+        button1.setStyleSheet("background-color: dodgerblue; color: white; font-weight: bold; ")
+        button1.setFont(QFont('Bodoni MT', 15))  # set font family and size
         button1.clicked.connect(self.toggle_filter_window)
         layout_v.addWidget(lbl)
         layout_v.addWidget(button1, alignment=Qt.AlignTop)
